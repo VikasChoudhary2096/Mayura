d8fbe2931755cbfe8fe6640489ce560cd44db154
Vikas Choudhary <vikas@bigphi.com>
2021-04-05 16:29:48 +0530

    signup form added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
@@ -10,6 +10,7 @@ import { AccountComponent } from './components/account/account.component';
 import { TransactionComponent } from './components/transaction/transaction.component';
 import { HomeComponent } from './components/home/home.component';
 import { MaterialModule } from './shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,9 @@ import { MaterialModule } from './shared/material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    ReactiveFormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/material.module.ts
+++ frontend/src/app/shared/material.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
 
 const materialModules = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatToolbarModule,
+  MatFormFieldModule
 ];
 
 @NgModule({
